3740e58787495a69b503e689b71d79e45b1c7b2d
Chris503g <75570657+Chris503g@users.noreply.github.com>
2022-02-26 12:08:01 +0700

    Add all data to repo

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+function getHome() {
+  var element = document.getElementById("home");
+  element.scrollIntoView();
+}
+
+function getIntro() {
+  var element = document.getElementById("intro");
+  element.scrollIntoView();
+}
+
+function getWork() {
+  var element = document.getElementById("work");
+  element.scrollIntoView();
+}
+
+function getInstrument() {
+  var element = document.getElementById("instrument");
+  element.scrollIntoView();
+}
